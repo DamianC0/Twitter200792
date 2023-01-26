4e7238abd6f97cbedc2684f4ba03a835f4ad3de0
DamianC0 <damian.cuper98@gmail.com>
2023-01-26 13:31:05 +0100

    Redirect back to main page after tweeting

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 from .forms import TweetForm
@@ -10,9 +10,12 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url != None:
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context = {"form": form})
 
