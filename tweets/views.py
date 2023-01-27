28ad2e548fc51dbc37352de3aaa5bf6aa359affa
DamianC0 <damian.cuper98@gmail.com>
2023-01-27 10:30:21 +0100

    Django REST

--- tweets/views.py
+++ tweets/views.py
@@ -1,47 +1,42 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
 from django.conf import settings
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Tweet
 from .forms import TweetForm
-from.serializers import TweetSerializer
+from .serializers import TweetSerializer
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hello world!</h1>")
     return render(request, "pages/home.html", context = {}, status = 200)
 
+#/create-tweet
+@api_view(['POST'])
 def tweet_create_view(request, *args, **kwargs):
-    serializer = TweetSerializer(data = request.POST or None)
-    if serializer.is_valid():
+    serializer = TweetSerializer(data = request.POST)
+    if serializer.is_valid(raise_exception=True):
         obj = serializer.save(user = request.user)
         next_url = request.POST.get("next") or None
         if next_url != None:
             return redirect(next_url)
-        return JsonResponse(serializer.data, status = 201)
-    return JsonResponse({}, status = 400)
+        return Response(serializer.data, status = 201)
+    return Response({}, status = 400)
 
 #/tweets
+@api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
-    data = {
-        "response": tweets_list
-    }
-    return JsonResponse(data)
+    serializer = TweetSerializer(qs, many = True)
+    return Response(serializer.data)
 
 #/tweets/{id}
+@api_view(['GET'])
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    data = {
-        "id": tweet_id,
-        #"image_path": obj.image.url,
-    }
-    status = 200
-
-    try:
-        obj = Tweet.objects.get(id = tweet_id)
-        data['content'] = obj.content
-    except:
-        data['message'] = "Not Found"
-        status = 404
-
-    return JsonResponse(data, status=status)
+    qs = Tweet.objects.filter(id = tweet_id)
+    if not qs.exists():
+        return Response({}, status = 404)
+    obj = qs.first()
+    serializer = TweetSerializer(obj)
+    return Response(serializer.data, status = 200)
