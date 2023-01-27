12760eb4b5ce7dbf4607396b6c0b8901128c9d77
DamianC0 <damian.cuper98@gmail.com>
2023-01-27 11:22:37 +0100

    Accounts creation

--- Twitter/settings.py
+++ Twitter/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     
     'tweets',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_RENDERER_CLASSES = [
+    'rest_framework.renderers.JSONRenderer',
+]
+
+if DEBUG:
+    DEFAULT_RENDERER_CLASSES += [
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
+}
--- Twitter/urls.py
+++ Twitter/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from tweets.views import home_view, tweet_detail_view, tweet_list_view, tweet_create_view
+from tweets.views import home_view, tweet_detail_view, tweet_list_view, tweet_create_view, tweet_delete_view
+from accounts.views import login_view, logout_view, register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
-    path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('tweets/<int:tweet_id>/', tweet_detail_view),
     path('tweets/', tweet_list_view),
     path('create-tweet/', tweet_create_view),
+    path('tweets/<int:tweet_id>/delete/', tweet_delete_view),
+
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('register/', register_view),
 ]
